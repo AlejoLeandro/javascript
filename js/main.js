2816669197182686c689196f6dfaacf8a6fe6c7f
alejoavendaño <alejo777aven@gmail.com>
2025-09-22 17:03:45 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+function bienvenida(nombre) {
+    console.log("Hola " + nombre , "esta es una pagina dedicada a Motos")
+}
+bienvenida("Jesus")
+
+//array
+let marcas = ["honda", "yamaha", "ducati", "ktm","motomel", "susuki"]
+console.log(marcas[4])
